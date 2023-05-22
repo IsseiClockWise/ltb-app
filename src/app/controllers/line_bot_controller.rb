1889fdb4ba5a9f1ea3d896aa39cee2b7efb077fc
IsseiClockWise <isseitakeda@clockwisesystem.com>
2023-05-22 08:46:04 +0000

    修正

---
+++ src/app/controllers/line_bot_controller.rb
@@ -0,0 +1,40 @@
+require 'line/bot'
+
+class LineBotController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+
+    head :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV['YOUR_CHANNEL_SECRET']
+      config.channel_token = ENV['YOUR_CHANNEL_TOKEN']
+    }
+  end
+end
